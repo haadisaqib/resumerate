01f8e8a55c214aabc9c6f189827ee57e0df6dc29
Haadi Saqib <haadi.saqib@gmail.com>
2023-11-02 13:28:11 -0500

    Descriptive commit message

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,10 +1,29 @@
 // src/InputForm.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const InputForm = () => {
   const [uploadCount, setUploadCount] = useState(3);
+
+  useEffect(() => {
+    let userUUID = localStorage.getItem('userUUID');
+
+    if (!userUUID) {
+      userUUID = uuidv4();
+      localStorage.setItem('userUUID', userUUID);
+      localStorage.setItem(`uploadCount_${userUUID}`, '3');
+    } else {
+      const savedUploadCount = localStorage.getItem(`uploadCount_${userUUID}`);
+      if (savedUploadCount) {
+        setUploadCount(parseInt(savedUploadCount, 10));
+      }
+    }
+
+    if (parseInt(uploadCount, 10) === 0) {
+      document.getElementById('submitBtn').disabled = true;
+    }
+  }, [uploadCount]);
 
   const handleUpload = () => {
     const userUUID = localStorage.getItem('userUUID');
@@ -16,6 +35,10 @@ const InputForm = () => {
       localStorage.setItem(`uploadCount_${userUUID}`, newUploadCount.toString());
     }
 
+    if (newUploadCount === 0) {
+      document.getElementById('submitBtn').disabled = true;
+    }
+
     // TODO: Add logic to make the API call here
   };
 
@@ -25,7 +48,7 @@ const InputForm = () => {
       <input type="file" accept=".png, .jpg, .jpeg, .pdf" />
       <br />
       <p>You have {uploadCount} submits left.</p>
-      <button onClick={handleUpload} disabled={uploadCount === 0}>
+      <button id="submitBtn" onClick={handleUpload} disabled={uploadCount === 0}>
         Submit
       </button>
     </div>
