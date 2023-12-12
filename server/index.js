70890c390b885aced6368d35c38b78b0cf11ac50
haadisaqib <haadi.saqib@gmail.com>
2023-12-12 16:51:22 -0600

    faultly API POST: FIX

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
-const app = require('express')();
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
 const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log("you are gay")
-)
+// Configure Multer for file uploads
+const storage = multer.memoryStorage(); // Store files in memory
+const upload = multer({ storage: storage });
+
+app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
+
+app.use(cors());
+
+// POST endpoint for handling file uploads
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Access the uploaded file from req.file
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // Handle the file as needed (e.g., save to disk, process, etc.)
+  // For demonstration purposes, log the file information
+  console.log('File received:', {
+    originalname: file.originalname,
+    mimetype: file.mimetype,
+    size: file.size,
+  });
+
+  // Send a response to the client
+  res.status(200).send('File uploaded successfully.');
+});
