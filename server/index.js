a8773562bc2ef77f7fc779b32e30b2499cda45a9
haadisaqib <haadi.saqib@gmail.com>
2024-01-02 17:19:31 -0600

    updated README and OPENAI API

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const Tesseract = require('tesseract.js');
-const axios = require('axios');
 const pdfParse = require('pdf-parse');
+const { OpenAI } = require('openai');
 
 require('dotenv').config();
+
+//initialize the client
+const openai = new OpenAI({apiKey: `${process.env.OPENAI_GPT_KEY}`});
 
 const app = express();
 const PORT = 8080;
@@ -42,21 +45,28 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     // Log the extracted text
     console.log('Extracted Text:', extractedText);
 
-    // Call OpenAI GPT API for tips and rating
-    const gptApiResponse = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      prompt: "say apple", // add extractedText
-      max_tokens: 150, // You may adjust this based on your response length requirements
-    }, {
-      headers: {
-        Authorization: `Bearer ${process.env.OPEN_AI_GPT_KEY}`, 
-        'Content-Type': 'application/json',
-      },
+    // Call OpenAI GPT API for tips and rating (TODO)
+    const response = await openai.chat.completions.create({
+      messages: [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": `Your job is to rate resumes on a scale of 1-10, and provide tips to improve the essay. Return response in the following parsable JSON format:
+
+        {
+            "R": "Rating",
+            "T": "Response"
+        }`},
+
+        {"role": "system", "content": "Ok, please provide the resume and I will be analyzing it. I will format my response in a parsable JSON format"},
+        {"role": 'user', "content": `Extract tips and rating from the following text: ${extractedText}`}],
+      model: 'gpt-3.5-turbo',
     });
 
-    // Extract tips and rating from the OpenAI GPT API response
-    const { choices } = gptApiResponse.data;
-    const gptTips = choices.map(choice => choice.text).join('\n');
+    // Extract tips and rating from the OpenAI GPT API response (TODO)
+    console.log(response.text);
 
+    const gptTips = response.choices[0].text;
+
+    console.log('GPT Tips:', gptTips);
     // Send the extracted text and tips as a response to the client
     res.status(200).json({ text: extractedText, tips: gptTips });
   } catch (error) {
